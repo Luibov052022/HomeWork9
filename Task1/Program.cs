dce12f311cd1c54d0c93e00938d1e82522c721b0
Liubov <pandora1705@mail.ru>
2022-07-05 15:41:27 +0400

    9е домашнее задание

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+
+void PrintNatural(int M, int N)
+{
+    if (M > N) return;
+    if (M % 2 == 0)
+    {
+        System.Console.Write(M + " ");
+    }
+    PrintNatural(M + 1, N);
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+PrintNatural(Prompt("Введите число 1: "), Prompt("Введите число 2: "));
+
---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+
+int SumNatural(int M, int N)
+{
+    if (M > N) return 0;
+
+    return M + SumNatural(M + 1, N);
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine($"Сумма натуральных элементов в промежутке равна {SumNatural(Prompt("Введите число 1: "), Prompt("Введите число 2: "))}");
+
